dc5e480ce7366b4f9a8e81891e6892caf1214207
lujo18 <lukej12805@gmail.com>
2025-11-01 13:13:28 -0500

    Remove Contentlayer - fix build scripts and Next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const { withContentlayer } = require('next-contentlayer')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {},
 }
 
-module.exports = withContentlayer(nextConfig)
+module.exports = nextConfig
